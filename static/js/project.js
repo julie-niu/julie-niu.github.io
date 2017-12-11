1078605c353a1fad32a29b786cf0751b91f53ca8
kevin-f-liu <kevin.f.liu2012@gmail.com>
2017-12-10 22:06:36 -0500

    React prject page

---
+++ static/js/project.js
@@ -0,0 +1,52 @@
+/**
+ * @file Class representing project content for both project list and modals
+ */
+
+// Global project name list
+var projects = ['One', 'Two', 'Three'];
+
+
+class ProjectModal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+
+    }
+}
+
+class ProjectTile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {projects: projects.slice(), selected: null};
+
+        this.selectProjectCallback = this.selectProjectCallback.bind(this);
+    }
+
+    selectProjectCallback(event) {
+        console.log(event);
+    }
+
+    render() {
+        let projectElements = this.state.projects.map((proj) => 
+            <h1 id="proj" onClick={this.selectProjectCallback}>{proj}</h1>
+        );
+
+        return (
+            <div className="row">
+                {projectElements}
+            </div>
+        );
+    }
+}
+
+
+$(document).ready(function() {
+    ReactDOM.render(
+        <ProjectTile></ProjectTile>,
+        document.getElementById('project-list-root')
+    );
+})
+

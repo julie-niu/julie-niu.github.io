f756953dacf1411537a62f810a775b5620c6fa65
kevin-f-liu <kevin.f.liu2012@gmail.com>
2017-12-11 22:32:07 -0500

    Fix some react

--- static/js/project.js
+++ static/js/project.js
@@ -3,17 +3,45 @@
  */
 
 // Global project name list
-var projects = ['MicroInstitution', 'StudentHousing'];
+var projects = {'MicroInstitution': {
+        title: 'The Urban Library',
+        subtitle: 'Micro-Institution'
+    }, 
+    'StudentHousing': {
+        title: 'The Modern Village',
+        subtitle: 'Student Housing'
+    },
+    'Oasis': {
+        title: 'The Astronomer\'s House',
+        subtitle: 'Oasis'
+    },
+    'PotteryStudio': {
+        title: 'The Pottery Studio',
+        subtitle: 'Artist\'s Residence'
+    },
+    'WishingTree': {
+        title: 'The Wishing Tree',
+        subtitle: 'Steel Assemblage'
+    }
+};
+
 
 
 class ProjectModal extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {current: null, projects: projects.slice()};
+    }
+
+    componentWillReceiveProps(nextProps) {
+        var selectedProj = Cookies.get('selectedProject');
+        if (this.state.current != selectedProj) {
+            this.setState({current: selectedProj});
+        }
     }
 
     render() {
-        return <h1>{Cookies.get('selectedProject')}</h1>;
+        return <h1>{this.state.projects[this.state.current].subtitle}</h1>;
     }
 }
 
@@ -27,16 +55,17 @@ class ProjectTile extends React.Component {
 
     selectProjectCallback(event, proj) {
         Cookies.set('selectedProject', proj);
+        this.setState({selected: proj});
         ReactDOM.render(<ProjectModal></ProjectModal>, document.getElementById('project-root'));
     }
 
     render() {
-        let projectElements = this.state.projects.map((proj) => 
+        let projectElements = Object.keys(this.state.projects).map((proj) => 
             <div className="col-md-3 col-sm-6 portfolio-item">
                 <a className="portfolio-link" onClick={(e) => this.selectProjectCallback(e, proj)} data-toggle="modal" href="#portfolioModal">
                     <div className="portfolio-hover">
                         <div className="portfolio-hover-content">
-                            <h1>{proj}</h1>
+                            <p className="project-name">{this.state.projoects[proj].title}</p>
                         </div>
                     </div>
                     <img className="img-fluid project-tile" src={"media/icons/" + proj + ".jpg"} alt=""/>
